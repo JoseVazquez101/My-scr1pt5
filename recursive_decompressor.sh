ec83f01b4e33dd57bddf34159e8ba7cbdafda777
JoseVazquez101 <111292579+JoseVazquez101@users.noreply.github.com>
2023-11-10 21:59:03 -0600

    Create recursive_decompressor.sh

---
+++ recursive_decompressor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+first_file_name="data.gz"
+decompressed_file_name="$(7z l data.gz | tail -n 3 | head -n 1 | awk 'NF{print $NF}')"
+
+7z x $first_file_name &>/dev/null
+
+while [ $decompressed_file_name ]; do
+  echo -e "\n[+] Nuevo archivo descomprimido: $decompressed_file_name"
+  7z x $decompressed_file_name &>/dev/null
+  decompressed_file_name="$(7z l $decompressed_file_name 2>/dev/null | tail -n 3 | head -n 1 | awk 'NF{print $NF}')"
+done
