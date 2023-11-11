8b19788a4bebe2ab76d23e996da7f3f072549205
JoseVazquez101 <111292579+JoseVazquez101@users.noreply.github.com>
2023-11-10 22:14:23 -0600

    Update myports_proc.sh

--- myports_proc.sh
+++ myports_proc.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-function myports() {
 echo
 cat /proc/net/tcp | uniq -u | awk 'NR > 1 {print $2}' | awk -F ":" '{print $2}' | while read line; do
     if [[ $line != "0" ]]; then
         echo -e "[+] Puerto $((16#$line)) abierto"
     fi
 done
-}
 
-myports
+read -p "Ver información de puertos (s/n): " op
+
+if [ "$op" = "s" ]; then
+    read -p "Puerto: " port
+    lsof -i :"$port"
+fi
