85f14de001b4a2be9124c9f0670e6ec12bffc807
JoseVazquez101 <111292579+JoseVazquez101@users.noreply.github.com>
2023-11-10 22:18:50 -0600

    Create webcrypt.sh

---
+++ webcrypt.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+red='\033[1;31m'
+orange='\033[1;38;5;208m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+aqua='\033[1;36m'
+white='\033[1;37m'
+gray='\033[1;30m'
+normal='\033[0m'
+
+echo -ne "\n\n"
+echo -e "${red}██     ██╗███████╗██████╗  ██████╗██████╗ ██╗   ██╗██████╗ ████████╗${normal}" && sleep 0.2
+echo -e "${red}██║    ██║██╔════╝██╔══██╗██╔════╝██╔══██╗╚██╗ ██╔╝██╔══██╗╚══██╔══╝${normal}" && sleep 0.2
+echo -e "${red}██║ █╗ ██║█████╗  ██████╔╝██║     ██████╔╝ ╚████╔╝ ██████╔╝   ██║   ${normal}" && sleep 0.2
+echo -e "${red}██║███╗██║██╔══╝  ██╔══██╗██║     ██╔══██╗  ╚██╔╝  ██╔═══╝    ██║   ${normal}" && sleep 0.2
+echo -e "${red}╚███╔███╔╝███████╗██████╔╝╚██████╗██║  ██║   ██║   ██║        ██║   ${normal}" && sleep 0.2
+echo -e "${red} ╚══╝╚══╝ ╚══════╝╚═════╝  ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝        ╚═╝   ${normal}" && sleep 0.2
+echo
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⣀⡀⢀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣷⡀⢿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⡿⠿⠿⠿⠿⢿⣿⣧⠈⢿⣿⣿⡄⠀⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣤⣤⣤⣤⣤⣼⣿⣿⣧⠈⢿⣿⣿⡄⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣦⣤⣤⣤⣤⣼⣿⣿⣿⡇⢈⣉⣉⣉⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⢸⣿⣿⡏⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⣿⣿⣿⠃⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢰⣿⣿⡟⠀⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣼⣿⣿⠇⠀⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀Made by: Retr0${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠃⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀Version: 1 .0.0${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⡿⠀⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀${normal}"
+echo -e "${orange}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠛⠛⠛⠛⠛⠀⠘⠛⠛⠀⠀⠀⠀${normal}"
+echo -ne "\n\n"
+echo -ne "${green}[-]${normal} ${white}Archivo de clave publica: ${normal}" && read data
+
+py_load=$(cat << EOL
+from Crypto.PublicKey import RSA
+
+with open('$data', 'r') as f:
+    pubkey = RSA.import_key(f.read())
+
+print("\\n[+] EL modulo n de la clave RSA es: ", pubkey.n)
+print("\\n[+] Formato HEX -> ",hex(pubkey.n))
+EOL
+)
+
+echo "$py_load" > 14-Data_Format_transparency.py
+python3 14-Data_Format_transparency.py
+
+hash=$(openssl pkey -outform der -pubin -in "$data" | sha256sum | tr -d '-')
+echo -ne "\n\n${blue}[+]${normal} ${white}Hash id 256 de certificado:${white} ${gray}${hash}${normal}"
+echo -ne "\n${purple}[*]${normal} ${gray}Comprobando si el certificado existe...${normal}"
+sleep 3
+curl -s -X GET "https://search.censys.io/search?resource=certificates&q="+$hash > resp.txt
+
+if cat resp.txt | grep -q 302; then
+  redirect=$(cat resp.txt | grep -A 7 "The resource" | head -1 | sed -e 's/^.*The resource was found at //;s/;.*$//')
+  echo -ne "\n${green}[+]${normal} ${white}El certificado existe. Redirigiendo...${normal}"
+  sleep 3
+  firefox $redirect & disown
+else
+  echo -ne "\n${red}[!] El certificado no existe.${normal}"
+  exit 1
+fi
+
+rm resp.txt
+exit 0
