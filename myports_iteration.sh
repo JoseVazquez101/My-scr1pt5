07f4ede30bd5a234cbc27bb5d7295f0a6565c7a0
JoseVazquez101 <111292579+JoseVazquez101@users.noreply.github.com>
2023-12-12 16:49:23 -0600

    Update myports_iteration.sh

--- myports_iteration.sh
+++ myports_iteration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for port in $(seq 1 65535); do
-   (echo '' > /dev/tcp/127.0.0.1/$port) 2>/dev/null && echo "Puerto $port abierto"
+   echo '' > /dev/tcp/127.0.0.1/$port) 2>/dev/null && echo "Puerto $port abierto"
 done
